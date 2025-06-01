f2302b26ebcc65f1a3809d376d957c4d0d36865b
B1ue-Te4 <103427649+B1ue-Te4@users.noreply.github.com>
2025-06-01 13:52:21 +0000

    bug

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,4 @@
-import { fetchIncoicesPages } from '@/app/lib/data';
+import { fetchInvoicesPages } from '@/app/lib/data';
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
