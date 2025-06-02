684f9cfd53c532bc60e04d48b3fece839c8047d8
B1ue-Te4 <103427649+B1ue-Te4@users.noreply.github.com>
2025-06-02 13:52:32 +0000

    cmt

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    ppr: 'incremental'
-  }
-};
+const nextConfig = {
+    experimental: {
+      ppr: 'incremental', // mod for Chapter 10
+      serverActions: {
+        allowedOrigins: ["https://stunning-train-6xxqrxxwxxx25px6.github.dev/stunning-train-6xxqrxxwxxx25px6.github.dev", "localhost:3000"],
+    }, // Added for 
+    },
+  }; 
 
 export default nextConfig;
