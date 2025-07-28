16602c29e3ad4a60f326f457bafd7fe1b5ddd812
B1ue-Te4 <103427649+B1ue-Te4@users.noreply.github.com>
2025-07-28 23:32:51 +0000

    change login-form.tsx

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -40,7 +40,7 @@ export default function LoginForm() {
                 id="email"
                 type="email"
                 name="email"
-                placeholder="user@nextmail.com"
+                placeholder="input user@nextmail.com"
                 required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -59,7 +59,7 @@ export default function LoginForm() {
                 id="password"
                 type="password"
                 name="password"
-                placeholder="123456"
+                placeholder="input 123456"
                 required
                 minLength={6}
               />
